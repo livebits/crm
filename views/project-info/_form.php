1b1039d3746bb279fffaa01754dc15984c4b74cc
Ahmad khalilfar <ahmad.khalilfar@gmail.com>
2018-10-15 09:26:21 +0330

    work on upload module

--- views/project-info/_form.php
+++ views/project-info/_form.php
@@ -31,18 +31,92 @@ use yii\widgets\ActiveForm;
         ]
     ); ?>
 
-    <?= $form->field($model, 'project_id')->dropDownList(
-        $projects
-    );
-    ?>
+    <?= $form->field($model, 'project_id')
+        ->widget(\kartik\select2\Select2::className(), ['data' => $projects,  'options' => ['dir' => 'rtl', 'placeholder' => 'پروژه مورد نظر را انتخاب کنید']]) ?>
 
     <?= $form->field($model, 'publish_version')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'package_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'sign_file')->fileInput([]) ?>
+<!--    --><?//= $form->field($model, 'sign_file')->fileInput([]) ?>.
+    <label>فایل Sign</label>
+    <?= \kartik\file\FileInput::widget([
+        'name' => 'ProjectInfo[sign_file]',
+        'id' => 'projectinfo-sign_file',
+        'options'=>[
+            'multiple'=>false,
+        ],
+        'pluginOptions' => [
+            'overwriteInitial'=>true,
+            'maxFileSize'=>10000,
+//            'allowedFileExtensions'=>['jpg', 'png'],
+            'showCaption' => false,
+            'showRemove' => true,
+            'showUpload' => false,
+            'browseClass' => 'btn btn-success btn-sm',
+            'removeClass' => 'btn btn-danger btn-sm',
+            'maxFileCount' => 1,
+        ]
+    ]); ?>
+    <?php
+        if(!$model->isNewRecord) {
+            if($model->sign_file) {
+                ?>
 
-    <?= $form->field($model, 'keystore')->fileInput([]) ?>
+                <div class="panel panel-info">
+                    <!--                <div class="panel-heading"></div>-->
+                    <div class="panel-body">
+                        <a target="_blank"
+                           href="<?= Yii::$app->homeUrl . 'media/project/attachments/' . $model->sign_file ?>">
+                            <?= explode('_', $model->sign_file)[2] ?>
+                        </a>
+                    </div>
+                </div>
+
+                <?php
+            }
+        }
+    ?>
+
+<!--    --><?//= $form->field($model, 'keystore')->fileInput([]) ?>
+    <label style="margin-top: 20px">رمز Keystore</label>
+    <?= \kartik\file\FileInput::widget([
+        'name' => 'ProjectInfo[keystore]',
+        'id' => 'projectinfo-keystore',
+        'options'=>[
+            'multiple'=>false,
+        ],
+        'pluginOptions' => [
+            'overwriteInitial'=>true,
+            'maxFileSize'=>10000,
+//            'allowedFileExtensions'=>['jpg', 'png'],
+            'showCaption' => false,
+            'showRemove' => true,
+            'showUpload' => false,
+            'browseClass' => 'btn btn-success btn-sm',
+            'removeClass' => 'btn btn-danger btn-sm',
+            'maxFileCount' => 1,
+        ]
+    ]); ?>
+    <?php
+    if(!$model->isNewRecord) {
+        if($model->keystore) {
+            ?>
+
+            <div class="panel panel-info">
+                <!--                <div class="panel-heading"></div>-->
+                <div class="panel-body">
+                    <a target="_blank"
+                       href="<?= Yii::$app->homeUrl . 'media/project/attachments/' . $model->keystore ?>">
+                        <?= explode('_', $model->keystore)[2] ?>
+                    </a>
+                </div>
+            </div>
+
+            <?php
+        }
+    }
+    ?>
 
     <?= $form->field($model, 'api_key')->textInput(['maxlength' => true]) ?>
 
